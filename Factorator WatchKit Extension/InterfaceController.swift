5f6f1772294ec2716e2703692378acfb7e0d4e7b
tidalboot <tidalboot@googlemail.com>
2015-07-09 14:06:39 +0100

    Added WatchKit Extension and app

---
+++ Factorator WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  Factorator WatchKit Extension
+//
+//  Created by Nick Jones on 09/07/2015.
+//  Copyright (c) 2015 Nick Jones. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
